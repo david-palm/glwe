978eef273cabc8e7494bfd8e20f23db5af368de1
david-palm <david.palm1996@gmail.com>
2022-07-26 14:29:56 +0200

    Set up GLFW and GLAD
    Added GLFW as a submodule and added GLAD ES 2 subset as libraries

--- cmake-build-debug/vendor/glfw/src/cmake_install.cmake
+++ cmake-build-debug/vendor/glfw/src/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/projects/uml-creator
+# Install script for directory: C:/projects/uml-creator/vendor/glfw/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,23 +37,7 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.1.3/bin/mingw/bin/objdump.exe")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("C:/projects/uml-creator/cmake-build-debug/vendor/glfw/cmake_install.cmake")
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/projects/uml-creator/cmake-build-debug/vendor/glfw/src/libglfw3.a")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("C:/projects/uml-creator/cmake-build-debug/vendor/glad/cmake_install.cmake")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/projects/uml-creator/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
