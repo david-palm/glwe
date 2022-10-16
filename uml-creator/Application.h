c1b7410268cf75263065a7d7e19db60e062b8e0b
david-palm <david.palm1996@gmail.com>
2022-10-16 15:05:38 +0200

    First working application class

--- uml-creator/Application.h
+++ uml-creator/Application.h
@@ -1,8 +1,30 @@
 #pragma once
 
-class Application {
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#define GLFW_INCLUDE_ES3
+#include "GLFW/glfw3.h"
+#else
+#include "GLFW/glfw3.h"
+#include "glad/glad.h"
+#endif
+
+#include "Renderer/Shader.h"
+#include "Renderer/Buffer.h"
+#include "Renderer/VertexArray.h"
+
+void mainLoop();
+
+class Application{
 public:
     Application();
     ~Application();
+    static Application& get() { return *s_Instance; }
+    void runLoop();
+private:
     void run();
+private:
+    static Application* s_Instance;
+    bool m_Running = false;
+    GLFWwindow* window;
 };
