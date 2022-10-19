10d354f506ebd25670c704f98525ca77b124194d
david-palm <david.palm1996@gmail.com>
2022-10-19 14:22:55 +0200

    Added virtual window class

---
+++ uml-creator/Window.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Events/Event.h"
+
+struct WindowProperties
+{
+    std::string title;
+    unsigned int width;
+    unsigned int height;
+
+    WindowProperties(const std::string& title = "UML Creator", unsigned int width = 1280, unsigned int height = 720)
+        : title(title), width(width), height(height)
+    {}
+};
+
+class Window
+{
+public:
+    using EventCallbackFunction = std::function<void(Event&)>;
+
+    virtual ~Window() {}
+
+    virtual void onUpdate() = 0;
+    virtual unsigned int getWidth() const = 0;
+    virtual unsigned int getHeight() const = 0;
+
+    virtual void setEventCallback(const EventCallbackFunction& callback) = 0;
+    virtual void setVSync(bool enabled) = 0;
+    virtual bool isVSync() const = 0;
+    static Window* create(const WindowProperties& properties = WindowProperties());
+
+};
